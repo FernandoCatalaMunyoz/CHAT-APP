eafca66272f148d90367e6a681b7b9df8b601a8a
FernandoCatalaMunyoz <108887947+FernandoCatalaMunyoz@users.noreply.github.com>
2022-11-29 19:05:54 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,49 +1,74 @@
-let IdCounter =0;
-let IdCounter2 =0;
+let IdCounter = 0;
+let IdCounter2 = 0;
+
+const channels = [];
 const input = document.querySelector('input[type="text"]');
-const input2 = document.querySelector('input[type="text"]');
-canalInput = document.getElementById('canalInput');
-mensajeInput = document.getElementById('mensajeInput');
+
+canalInput = document.getElementById("canalInput");
 
 
 
-canalInput.addEventListener('submit', ()=>{
-    addCanal();
+
+canalInput.addEventListener("submit", () => {
+  addCanal();
 });
 
-let addCanal = ()=>{
+let addCanal = () => {
   IdCounter++;
-  
+  console.log(input.value);
   let newValue = input.value;
-
-  list.innerHTML += `<div class="canal-container" id="${IdCounter}">
-  <label>
-      <input type="image" src="/CHAT-APP/imagenes/live.png" id=imagenUser>
-        ${newValue}
-  </label>
-  <img src="/CHAT-APP/imagenes/basura.png" id="closecanal">
-</div>`
+  channels.push(newValue);
+  printChannels(channels);
 
   input.value = '';
-}
+};
 
-mensajeInput.addEventListener('submit', ()=>{    //aqui no se como seleccionar el segundo input
-  addMensaje();
-});
 
-let addMensaje = ()=>{
-IdCounter2++;
 
-let newValue2 = input2.value;
+const printChannels = (channels) => {
+  console.log(channels)
+  list.innerHTML = '';
+  channels.forEach(element => {
+    console.log(element)
+   list.innerHTML += `<div class="canal-container" id="${IdCounter}">
+     <label>
+         <input type="image" src="/CHAT-APP/imagenes/live.png" id=imagenUser>
+           ${element}
+     </label>
+     <img src="/CHAT-APP/imagenes/basura.png" id="closecanal">
+   </div>`;
+  });
+};
 
-list2.innerHTML += `<div class="list-container" id="${IdCounter2}">
+
+
+
+
+
+
+
+// Funcion añadir mensajes
+mensajeInput = document.getElementById("mensajeInput");
+
+let addMensaje = () => {
+  IdCounter2++;
+  let date = new Date();
+  
+  console.log(date.toLocaleTimeString(), date.toLocaleDateString());
+
+  let newValue2 = input2.value;
+
+  list2.innerHTML += `<div class="list-container" id="${IdCounter2}">
 <label>
     <input type="image" src="/CHAT-APP/imagenes/hombre.png" id=imagenUser>
       ${newValue2}
 </label>
 <img src="/CHAT-APP/imagenes/basura.png" id="closecanal">
-</div>`
+</div>`;
 
-input.newValue2 = '';
-}
+  input.newValue2 = "";
+};
+mensajeInput.addEventListener("submit", () => {
 
+  addMensaje();
+});
